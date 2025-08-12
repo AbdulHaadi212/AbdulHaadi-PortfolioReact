0617adf052da15e20f086315be6d05a2b4d0be59
Abdul Haadi <haadialiraj2003@gmail.com>
2025-08-13 02:47:20 +0500

    Update vertical timeline SCSS

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,29 +1,29 @@
 import React from "react";
 import "../assets/styles/Main.scss"; // Adjust path if needed
-import image1 from "../assets/images/image1.png";
-import image2 from "../assets/images/image2.png";
-import image3 from "../assets/images/image3.png";
-import image4 from "../assets/images/image4.png";
-import image5 from "../assets/images/image5.png";
-import image6 from "../assets/images/image6.png";
-import image7 from "../assets/images/AI1.jpg";
-import image8 from "../assets/images/AI2.jpg";
-import image9 from "../assets/images/AI3.jpg";
-import image10 from "../assets/images/AI4.jpg";
-import image11 from "../assets/images/AI5.jpg";
-import image12 from "../assets/images/AI6.jpg";
-import image13 from "../assets/images/Chat1.jpg";
-import image14 from "../assets/images/Chat2.jpg";
-import image15 from "../assets/images/Chat3.jpg";
-import image16 from "../assets/images/Cricket1.jpg";
-import image17 from "../assets/images/Cricket2.jpg";
-import image18 from "../assets/images/Cricket3.jpg";
-import image19 from "../assets/images/Movie1.png";
-import image20 from "../assets/images/Movie2.png";
-import image21 from "../assets/images/movie4.png";
-import image22 from "../assets/images/Insta1.png";
-import image23 from "../assets/images/Insta2.png";
-import image24 from "../assets/images/Insta3.png";
+import bgLight1 from "../assets/images/image1.png";
+import bgLight2 from "../assets/images/image2.png";
+import bgLight3 from "../assets/images/image3.png";
+import bgLight4 from "../assets/images/image4.png";
+import bgLight5 from "../assets/images/image5.png";
+import bgLight6 from "../assets/images/image6.png";
+import bgLight7 from "../assets/images/AI1.jpg";
+import bgLight8 from "../assets/images/AI2.jpg";
+import bgLight9 from "../assets/images/AI3.jpg";
+import bgLight10 from "../assets/images/AI4.jpg";
+import bgLight11 from "../assets/images/AI5.jpg";
+import bgLight12 from "../assets/images/AI6.jpg";
+import bgLight13 from "../assets/images/Chat1.jpg";
+import bgLight14 from "../assets/images/Chat2.jpg";
+import bgLight15 from "../assets/images/Chat3.jpg";
+import bgLight16 from "../assets/images/Cricket1.jpg";
+import bgLight17 from "../assets/images/Cricket2.jpg";
+import bgLight18 from "../assets/images/Cricket3.jpg";
+import bgLight19 from "../assets/images/Movie1.png";
+import bgLight20 from "../assets/images/Movie2.png";
+import bgLight21 from "../assets/images/movie4.png";
+import bgLight22 from "../assets/images/Insta1.png";
+import bgLight23 from "../assets/images/Insta2.png";
+import bgLight24 from "../assets/images/Insta3.png";
 
 
 
@@ -48,9 +48,9 @@ const App: React.FC = () => {
           <div className="media-item">
             <h3>Project Gallery</h3>
             <div className="image-row">
-              <img src={image7} alt="Project Image 1" className="image-left" />
-              <img src={image8} alt="Project Image 2" className="image-center" />
-              <img src={image9} alt="Project Image 3" className="image-right" />
+              <img src={bgLight7} alt="Project Image 1" className="image-left" />
+              <img src={bgLight8} alt="Project Image 2" className="image-center" />
+              <img src={bgLight9} alt="Project Image 3" className="image-right" />
             </div>
 
           </div>
@@ -64,9 +64,9 @@ const App: React.FC = () => {
           <div className="media-item">
             <h3>Project Gallery</h3>
             <div className="image-row">
-              <img src={image10} alt="Project Image 1" className="image-left" />
-              <img src={image11} alt="Project Image 2" className="image-center" />
-              <img src={image12} alt="Project Image 3" className="image-right" />
+              <img src={bgLight10} alt="Project Image 1" className="image-left" />
+              <img src={bgLight11} alt="Project Image 2" className="image-center" />
+              <img src={bgLight12} alt="Project Image 3" className="image-right" />
             </div>
 
           </div>
@@ -80,9 +80,9 @@ const App: React.FC = () => {
           <div className="media-item">
             <h3>Project Gallery</h3>
             <div className="image-row">
-              <img src={image13} alt="Project Image 1" className="image-left" />
-              <img src={image14} alt="Project Image 2" className="image-center" />
-              <img src={image15} alt="Project Image 3" className="image-right" />
+              <img src={bgLight13} alt="Project Image 1" className="image-left" />
+              <img src={bgLight14} alt="Project Image 2" className="image-center" />
+              <img src={bgLight15} alt="Project Image 3" className="image-right" />
             </div>
 
           </div>
@@ -96,9 +96,9 @@ const App: React.FC = () => {
           <div className="media-item">
             <h3>Project Gallery</h3>
             <div className="image-row">
-              <img src={image16} alt="Project Image 1" className="image-left" />
-              <img src={image17} alt="Project Image 2" className="image-center" />
-              <img src={image18} alt="Project Image 3" className="image-right" />
+              <img src={bgLight16} alt="Project Image 1" className="image-left" />
+              <img src={bgLight17} alt="Project Image 2" className="image-center" />
+              <img src={bgLight18} alt="Project Image 3" className="image-right" />
             </div>
 
           </div>
@@ -112,9 +112,9 @@ const App: React.FC = () => {
           <div className="media-item">
             <h3>Project Gallery</h3>
             <div className="image-row">
-              <img src={image22} alt="Project Image 1" className="image-left" />
-              <img src={image23} alt="Project Image 2" className="image-center" />
-              <img src={image24} alt="Project Image 3" className="image-right" />
+              <img src={bgLight22} alt="Project Image 1" className="image-left" />
+              <img src={bgLight23} alt="Project Image 2" className="image-center" />
+              <img src={bgLight24} alt="Project Image 3" className="image-right" />
             </div>
 
           </div>
@@ -128,9 +128,9 @@ const App: React.FC = () => {
           <div className="media-item">
             <h3>Project Gallery</h3>
             <div className="image-row">
-              <img src={image19} alt="Project Image 1" className="image-left" />
-              <img src={image20} alt="Project Image 2" className="image-center" />
-              <img src={image21} alt="Project Image 3" className="image-right" />
+              <img src={bgLight19} alt="Project Image 1" className="image-left" />
+              <img src={bgLight20} alt="Project Image 2" className="image-center" />
+              <img src={bgLight21} alt="Project Image 3" className="image-right" />
             </div>
 
           </div>
@@ -150,9 +150,9 @@ const App: React.FC = () => {
           <div className="media-item">
             <h3>Project Gallery</h3>
             <div className="image-row">
-              <img src={image1} alt="Project Image 1" className="image-left" />
-              <img src={image2} alt="Project Image 2" className="image-center" />
-              <img src={image3} alt="Project Image 3" className="image-right" />
+              <img src={bgLight1} alt="Project Image 1" className="image-left" />
+              <img src={bgLight2} alt="Project Image 2" className="image-center" />
+              <img src={bgLight3} alt="Project Image 3" className="image-right" />
             </div>
 
           </div>
@@ -166,9 +166,9 @@ const App: React.FC = () => {
           <div className="media-item">
             <h3>Project Gallery</h3>
             <div className="image-row">
-              <img src={image4} alt="Project Image 4" />
-              <img src={image5} alt="Project Image 5" />
-              <img src={image6} alt="Project Image 6" />
+              <img src={bgLight4} alt="Project Image 4" />
+              <img src={bgLight5} alt="Project Image 5" />
+              <img src={bgLight6} alt="Project Image 6" />
             </div>
           </div>
         </div>
